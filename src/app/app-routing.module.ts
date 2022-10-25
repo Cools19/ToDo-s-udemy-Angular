e95c25b2f46d2e7307ce20ca2febfb9ff59accd7
unknown <cogjava3106@iiht.tech>
2022-10-25 11:28:11 +0530

    RouteGuardService Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,14 @@ import { ListTodosComponent } from './Components/list-todos/list-todos.component
 import { LoginComponent } from './Components/login/login.component';
 import { LogoutComponent } from './Components/logout/logout.component';
 import { WelcomeComponent } from './Components/welcome/welcome.component';
+import { RouteGuardService } from './Services/route-guard.service';
 
 const routes: Routes = [
-  {path:'login',component:LoginComponent},
+  {path:'login',component:LoginComponent},// canActivate,RouteGuardService
   {path:'',component:LoginComponent},
-  {path:'welcome/:name',component:WelcomeComponent},
-  {path:'todos',component:ListTodosComponent},
-  {path:'logout',component:LogoutComponent},
+  {path:'welcome/:name',component:WelcomeComponent, canActivate:[RouteGuardService]},
+  {path:'todos',component:ListTodosComponent,canActivate:[RouteGuardService]},
+  {path:'logout',component:LogoutComponent,canActivate:[RouteGuardService]},
   
   {path:'**',component:ErrorComponent}
 ];
